ecfdff7c00cc65f2558b3375a48e9f1957e459ee
Luis Abreu <luis1abreu11@gmail.com>
2024-05-26 00:26:21 -0400

    Adding more functionality to it.

--- Banking/Banking.cpp
+++ Banking/Banking.cpp
@@ -7,19 +7,69 @@ class Bank
 private:
 	std::string m_accountHolderName;
 	double m_customerBalance;
-	int accountNumber;
+	int m_accountNumber;
 
 public:
 
-	void customerBalance(double balance)
+	void customerBalance()
 	{
-		std::cout << "Your currently have: " << balance << std::endl;
+		std::cout << "Your currently have: " << m_accountHolderName << std::endl;
+	}
+
+	Bank()
+	{
+		m_accountHolderName = "Empty";
+		m_customerBalance = 0;
+		m_accountNumber = 1;
+	}
+
+	std::string newUser()
+	{
+		std::cout << "Enter your full name: " << std::endl;
+		std::cin >> m_accountHolderName;
+		return m_accountHolderName;
+	}
+
+	int checkUserMembership()
+	{
+		std::string answer;
+		std::cout << "Do you have an account. yes/no: " << std::endl;
+		std::cin >> answer;
+		if (answer == "yes")
+		{
+			return true;
+		}
+		else {
+			std::cout << newUser();
+		}
+	}
+
+	int accountActionable()
+	{
+		return 0;
 	}
 };
+
+class AccountCreation
+{
+private:
+	std::string m_accountHolderName;
+	double m_customerID;
+	int m_accountNumber;
+
+public:
+	char* newUser()
+	{
+		std::cout << "Enter your full name";
+		std::cin >> m_accountHolderName;
+	}
+	
+};
+
 
 
 int main()
 {
 	Bank bank;
-	bank.customerBalance(30);
+	bank.checkUserMembership();
 }
